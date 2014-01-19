1830577deaf346b7b03d2b7c45042aafea72a5be
Tyler Robbins <tyler@familyrobbins.com>
2014-01-19 12:46:32 -0800

    Added more commands

--- FTPConnect.py
+++ FTPConnect.py
@@ -5,33 +5,57 @@
 #################
 
 import urllib2
+import os
 from ftplib import FTP
 from getpass import getpass
 
-ftp = FTP('ftp.tylerclay.com')
-
-user = raw_input(">>> UserName: ")
-passw = getpass(">>> Password: ")
-
 ulogin = True
+
+ftp = FTP('ftp.tylerclay.com') #connects to website
+
+user = raw_input(">>> UserName: ") #gets username
+passw = getpass(">>> Password: ") #gets password
 
 while ulogin:
 	try:
-		ftp.login(user, passw)
+		ftp.login(user, passw) #tries to input username and password
 		ulogin = False
-	except(Exception):
+	except(Exception): #catches login error
 		print ">>> Incorrect login."
 		exit()
 
 ftp.cwd('www.tylerclay.com')
-ftp.retrlines('LIST')
+#print "Permissions", "Date Updated", "Files"
+ftp.retrlines('LIST') #list all directories
 
 while True:
-	loc = raw_input('>>> ')
-	
-	if loc.upper() == 'EXIT':
-		exit()
+	loc = raw_input('>>> ') #user lists commands
+		
+	try:
+		if loc.upper() == 'EXIT': #exits program
+			exit()
 
-	ftp.cwd(loc)
-	ftp.retrlines('LIST')
+		elif loc.split(" ")[0] == 'cd':
+			loc = loc.split(" ")
 
+			try:
+				ftp.cwd(loc[1]) #changes directory to loc
+			except(Exception):
+				print "Could not find ", loc[1], ". Destination does not exist."
+		
+		elif loc == 'ls': #lists directories and files
+			ftp.retrlines('LIST')
+		
+		elif loc.split(" ")[0] == 'mkdir':
+			loc = loc.split(" ")
+
+			try:
+				os.mkdir(loc[1])
+
+			except(Exception):
+				print "Destination already exists"
+
+	except(Exception):
+		print loc , ": Command not found."
+
+nuclear = u'\u2622'
